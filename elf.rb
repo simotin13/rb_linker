4c364c0dac6dc263cb330deecb0caadaddc5ad81
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2017-02-20 03:01:07 +0900

    セクション名一覧出力

--- elf.rb
+++ elf.rb
@@ -1,106 +1,24 @@
+require './monkey_patch'
 require './machine_arch_list'
 
-class Array
-		def to_i(endian=:little)
-			case self.length
-			when 8
-				to_int64(endian)
-			when 4
-				to_int32(endian)
-			when 2
-				to_int16(endian)
-			when 1
-				self[0].to_i
-			else
-				# TODO exception...
-			end
-		end
-
-		# int(64bit)
-		def to_int64(endian=:little)
-			if endian == :big
-				(self[7].to_i << 56) +
-				(self[6].to_i << 48) +
-				(self[5].to_i << 40) +
-				(self[4].to_i << 32) +
-				(self[3].to_i << 24) +
-				(self[2].to_i << 16) +
-				(self[1].to_i << 8)  +
-				(self[0].to_i)
-			else
-				# TODO スワップさせる
-				self[0].to_i +
-				(self[1].to_i << 8) +
-				(self[2].to_i << 16) +
-				(self[3].to_i << 24) +
-				(self[4].to_i << 32) +
-				(self[5].to_i << 40) +
-				(self[6].to_i << 48) +
-				(self[7].to_i << 56)
-			end
-		end
-
-		# int(32bit)
-		def to_int32(endian=:little)
-			if endian == :big
-				(self[0].to_i << 24) + (self[1].to_i << 16) + (self[2].to_i << 8) + (self[3].to_i)
-			else
-				self[0].to_i + (self[1].to_i << 8) + (self[2].to_i << 16) + (self[3].to_i << 24)
-			end
-		end
-
-		# short(16bit)
-		def to_int16(endian=:little)
-			if endian == :big
-				self[1].to_i + (self[0].to_i << 8)
-			else
-				self[0].to_i + (self[1].to_i << 8)
-			end
-		end
-end
-
-class Fixnum
-  def to_h
-		# TODO Fixnumの値によって変えるべき。。。
-		if self < 0xFF
-			sprintf("0x%02X", self)
-  	elsif self < 0xFFFF
-			sprintf("0x%04X", self)
-		elsif self < 0xFFFFFFFF
-			sprintf("0x%08X", self)
-		elsif self < 0xFFFFFFFFFFFFFFFF
-			sprintf("0x%16X", self)
-  	else
-			# TODO
-			puts "error..."
-  	end
-  end
-end
-
 class ELF
-	ELF_32_HEADER_SIZE = 52
-	ELF_64_HEADER_SIZE = 64
+	SIZE_IDENT			= 16
+	SIZE_HARF_WORD	= 2
+	SIZE_WORD				= 4
+	SIZE_XWORD			= 8
+	SIZE_ADDR_32		= 4
+	SIZE_ADDR_64		= 8
+
 	def initialize filepath
 		@bin = File.binread(filepath).unpack("C*")
 		unless is_elf?
 			throw "This is not ELF Format File"
 		end
-
-		# ELF ヘッダの解析結果出力
-		read_header
+		@pos_entry = 24			# e_entry offset pos
+		read_elf_header
 	end
 
-	# identのマジックナンバーチェック
-	def is_elf?
-		@ident = @bin[0, 16]
-		return false if @ident[0] != 0x7F
-		return false if @ident[1] != 'E'.ord
-		return false if @ident[2] != 'L'.ord
-		return false if @ident[3] != 'F'.ord
-		true
-	end
-
-	def read_header
+	def read_elf_header
 		read_elf_class
 		read_endian
 		read_elf_version
@@ -112,11 +30,13 @@ class ELF
 		read_ph_offset
 		read_sh_offset
 		read_eflags
-		#read_elf_header_size
-		#read_ph_size
+		read_eh_size
+		read_ph_size
 		read_ph_num
+		read_sh_size
 		read_sh_num
 		read_shs_idx
+		read_sections
 	end
 
 	def read_elf_class
@@ -179,30 +99,17 @@ class ELF
 	end
 
 	def read_entry
-		if @elf_class == :class_32bit
-			# 32bit
-			@e_entry = @bin[24, @addr_size].bytes.to_i
-			@pos_phoff = 28
-			@pos_shoff = 32
+		@e_entry = @bin[@pos_entry, @addr_size].to_i
+		@pos_phoff = @pos_entry   + @addr_size
+		@pos_shoff = @pos_phoff  + @addr_size
+		@pos_eflags = @pos_shoff + @addr_size
+		@pos_ehsize = @pos_eflags + SIZE_WORD
+		@pos_phsize_offset = @pos_ehsize + SIZE_HARF_WORD
+		@pos_phnum_offset = @pos_phsize_offset + SIZE_HARF_WORD
+		@pos_shsize_offset = @pos_phnum_offset + SIZE_HARF_WORD
+		@pos_shnum_offset = @pos_shsize_offset + SIZE_HARF_WORD
+		@pos_shstrndx = @pos_shnum_offset + SIZE_HARF_WORD
 
-			# e_shoff(4) + e_flags(4) + e_ehsize(2) + e_phentsize(2)
-			@pos_phnum_offset = @pos_shoff + (4 + 4 + 2 + 2)
-
-			# phnum(2) + shentsize(2)
-			@pos_shnum_offset = @pos_phnum_offset + 2 + 2
-			@pos_shstrndx = @pos_shnum_offset + 2
-		else
-			# 64bit
-			@e_entry = @bin[24, @addr_size].to_i
-			@pos_phoff = 32
-			@pos_shoff = 40
-			# e_shoff(8) + e_flags(4) + e_ehsize(2) + e_phentsize(2)
-			@pos_phnum_offset = @pos_shoff + (8 + 4 + 2 + 2)
-
-			# phnum(2) + shentsize(2)
-			@pos_shnum_offset = @pos_phnum_offset + 2 + 2
-			@pos_shstrndx = @pos_shnum_offset + 2
-		end
 		puts "entry point address		:#{@e_entry.to_h}"
 	end
 
@@ -213,34 +120,92 @@ class ELF
 
 	def read_sh_offset
 		@e_shoff = @bin[@pos_shoff, @addr_size].to_i
-		puts "section header offset address	:#{@e_shoff.to_h}"
+		puts "section header offset address	:#{@e_shoff.to_h}(#{@e_shoff})"
 	end
 
 	def read_eflags
-			puts "e_flags				:Not Used..."
+			eflag = @bin[@pos_eflags, SIZE_WORD].to_i
+			puts "e_flags				:#{eflag.to_h}(Not Used)"
 	end
 
-	def show_elf_header_size
-		puts "elf header size	:#{ELF_32_HEADER_SIZE}"
+	def read_eh_size
+		@eh_size = @bin[@pos_ehsize, SIZE_HARF_WORD].to_i
+		puts "elf header size			:#{@eh_size}"
 	end
 
-	def show_prog_header_size
-		puts "program header size	:#{ELF_64_HEADER_SIZE}"
+	def read_ph_size
+		@ph_size = @bin[@pos_phsize_offset, SIZE_HARF_WORD].to_i
+		puts "program header size		:#{@ph_size}"
 	end
 
 	def read_ph_num
-		@e_phnum = @bin[@pos_phnum_offset, 2].to_i
-		puts "program header entry number	:#{@e_phnum.to_h}"
+		@e_phnum = @bin[@pos_phnum_offset, SIZE_HARF_WORD].to_i
+		puts "program header entry number	:#{@e_phnum}"
+	end
+
+	def read_sh_size
+		@sh_size = @bin[@pos_shsize_offset, SIZE_HARF_WORD].to_i
+		puts "section header size 		:#{@sh_size}"
 	end
 
 	def read_sh_num
-		@e_shnum = @bin[@pos_shnum_offset, 2].to_i
-		puts "section header entry number	:#{@e_shnum.to_h}"
+		@e_shnum = @bin[@pos_shnum_offset, SIZE_HARF_WORD].to_i
+		puts "section header entry number	:#{@e_shnum}"
 	end
 
 	def read_shs_idx
-		 @shs_idx = @bin[@pos_shstrndx, 2].to_i
-		 puts "section name strings section	:#{@shs_idx.to_h}"
+		 @shs_idx = @bin[@pos_shstrndx, SIZE_HARF_WORD].to_i
+		 puts "section name strings section	:#{@shs_idx}"
+	end
+
+	def read_sections
+		# セクション名保持セクションのオフセット取得
+		@s_names_offset = @e_shoff + ( (@shs_idx) * @sh_size)
+		puts "s_names_offset:#{@s_names_offset.to_h}"
+		sh_name = @bin[@s_names_offset, @sh_size]
+
+		# セクションのオフセット位置 格納位置
+		# sh_name(4) + sh_type(4) + sh_flags(4) + sh_addr(addr_size)
+		name_pos = 0
+		type_pos = name_pos + SIZE_WORD
+		sh_type = sh_name[type_pos, SIZE_WORD].to_i
+		flags_pos = type_pos + SIZE_WORD
+		sh_flags = sh_name[flags_pos, SIZE_WORD].to_i
+		puts "sh_type:#{sh_type}, sh_flags:#{sh_flags}"
+
+		if @elf_class == :class_32bit
+			flags_size = SIZE_WORD
+			sh_size_size = SIZE_WORD
+		else
+			flags_size = SIZE_XWORD
+			sh_size_size = SIZE_XWORD
+		end
+
+		addr_pos = flags_pos + flags_size
+		offset_pos = addr_pos + @addr_size
+		puts "offset_pos:#{offset_pos}"
+		sh_offset = sh_name[offset_pos, @addr_size].to_i
+		size_pos = offset_pos + @addr_size
+		sh_size = sh_name[size_pos, sh_size_size].to_i
+		puts "sh_offset:#{sh_offset.to_h}, sh_size:#{sh_size.to_h}"
+		names_section = @bin[sh_offset, sh_size]
+		pos = 0
+		lengh = names_section.length
+		until lengh <= pos do
+			name = names_section[pos, lengh].c_str
+			puts name
+			pos += name.length + 1
+		end
+	end
+
+	# identのマジックナンバーチェック
+	def is_elf?
+		@ident = @bin[0, SIZE_IDENT]
+		return false if @ident[0] != 0x7F
+		return false if @ident[1] != 'E'.ord
+		return false if @ident[2] != 'L'.ord
+		return false if @ident[3] != 'F'.ord
+		true
 	end
 end
 
