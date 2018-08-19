10e5675328888b842388aab7c5f0a00604a25e6b
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2018-08-19 23:08:07 +0900

    add loop code test.

--- test/gcc/func.c
+++ test/gcc/func.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 extern int ext_number;
 extern char *ext_srt;
 extern int ext_add(int , int);
@@ -20,4 +22,16 @@ void set_ext_number(void)
 {
     // set extern variable
     ext_number = 0x1234;
+}
+
+void loop(void)
+{
+    int a = 0;
+    while(1)
+    {
+      if (a % 10 == 0) {
+          printf("10");
+	  }
+      a++;
+    }
 }
