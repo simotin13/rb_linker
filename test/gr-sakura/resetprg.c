40fedcb373156bdc2e65ca49badda51a7a7e5a9a
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2018-08-20 00:17:32 +0900

    use stack, add SU, SI section.

--- test/gr-sakura/resetprg.c
+++ test/gr-sakura/resetprg.c
@@ -3,9 +3,13 @@
 #pragma section ResetPRG
 #pragma entry PowerON_Reset_PC
 
+// use stack
+#pragma stacksize su=0x100
+#pragma stacksize si=0x300
+
 void PowerON_Reset_PC(void) {
-    int i;
-    unsigned char reg; 
+    volatile int i;
+    volatile unsigned char reg; 
     PORTA.PDR.BIT.B0 = 1;
     while(1){
         if (i % 1000 == 0) {
